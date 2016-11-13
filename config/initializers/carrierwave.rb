f7abe0f8d691b8b4cd23f6958462f457eeede5ff
guyatic <guyatic@gmail.com>
2016-11-13 15:43:08 -0800

    x

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIH3IK25THNRJSLJQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :provider                         => 'Google',
+    :google_storage_access_key_id     => 'GOOGYJGMIV5A5CM3BYKT',
+    :google_storage_secret_access_key => 'MtT/KacIi0o3ovfm8c8UFQqQ1acwDr89AUvlYWuk'
   }
-  config.fog_directory  = 'yelpapp'                     # required
+  config.fog_directory = 'yelpapp'
 end
