b7347f059edfe94861a7b6de0c6df8f3e6532e3e
guyatic <guyatic@gmail.com>
2016-11-13 15:54:53 -0800

    Figaro setup.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.3.0'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem "fog"
 gem 'carrierwave'
+gem "figaro"
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider                         => 'Google',
-    :google_storage_access_key_id     => 'GOOGYJGMIV5A5CM3BYKT',
-    :google_storage_secret_access_key => 'MtT/KacIi0o3ovfm8c8UFQqQ1acwDr89AUvlYWuk'
+    :google_storage_access_key_id     => ENV["google_access_key_id"],
+    :google_storage_secret_access_key => ENV["google_secret_access_key"]
   }
-  config.fog_directory = 'yelpapp'
+  config.fog_directory = ENV["fog_directory"]
 end
